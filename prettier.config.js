dadff3de2697621986887d08fda69b817c1bc99a
yago-codev <yagomilano92@gmail.com>
2020-04-24 04:22:42 -0300

    Criando as Rotas da Aplicação

---
+++ prettier.config.js
@@ -0,0 +1,5 @@
+module.exports = {
+  singleQuote: true, // aspas simples,
+  trailingComma: "all", // vírgula no último elemento de objetos e arrays
+  allowParens: "avoid", // não vai colocar os parênteses em volta de arrow functions com apenas 1 parâmetro
+};
---
+++ src/pages/Repository/index.tsx
@@ -0,0 +1,7 @@
+import React from 'react';
+
+const Repository: React.FC = () => {
+  return <h1>Repositories</h1>;
+};
+
+export default Repository;
